6d18a328fd562123413c8ed294b855edbceda255
eranHe13 <83641304+eranHe13@users.noreply.github.com>
2021-06-14 18:55:11 +0300

    tmp

--- temp.c
+++ temp.c
@@ -1,3 +1,53 @@
+#include "matam.h"
+#include <stdio.h>
+#include <string.h>
+
+#ifndef CAR_PROJECT_BY_TREES_GENERIC_H
+#define CAR_PROJECT_BY_TREES_GENERIC_H
+
+typedef struct Node {
+    char *key;
+    void *data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+typedef struct Tree {
+    Node *root;
+    int elementCount;
+
+    void *(*init)();
+
+    int (*compare)(void *, void *);
+
+    void *(*get)(void *);
+
+    void (*freeData)(void *);
+    void (*printTree)(void* node);
+} Tree;
+typedef struct LinkedNode {
+    void *data;
+    struct LinkedNode *next;
+} LinkedNode;
+
+void *createTree(void *(*init)(), int (*compare)(void *data, void *root), void *(*get)(void *data),
+                 void freeData(void *data) ,void (*printTree)(void* node) );
+
+int addNewNode(Tree *tree);
+/**/
+int removeNode(Tree *tree, void *(*get)(void *data), int (*compare)(void *data, void *root));
+void *findNode(Tree *tree,void *(*get)(void *),int (*compare)(void *,void *));
+
+void freeTree(Tree* tree );
+void printTree(Node* node, void (*print)(void* Node));
+
+#endif
+
+
+
+
+
+
+
 
 void printTree(Node * node, void (*print)(void* Node)){
     Node *tmp = node;
